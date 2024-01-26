0f09e063c8765d83e3722a3e1783fe9f53729c5f
husniddin100 <uchanal10@gmail.com>
2024-01-26 15:35:13 +0500

    +profile+attach

--- src/main/java/com/example/dto/AttachDTO.java
+++ src/main/java/com/example/dto/AttachDTO.java
@@ -1,4 +1,11 @@
 package com.example.dto;
 
+import java.rmi.server.UID;
+
 public class AttachDTO {
+     private UID id;
+     private String original_name;
+     private String path;
+     private Long size;
+
 }
